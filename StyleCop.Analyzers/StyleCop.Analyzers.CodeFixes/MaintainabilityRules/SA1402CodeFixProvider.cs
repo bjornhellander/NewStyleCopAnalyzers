58ac7b649fdd7d27c1b064fd4b6ea1b200372af0
Matt Chaulklin <mchaulklin@gmail.com>
2025-08-05 14:46:54 -0400

    Moved tests to correct class. Added new code fix

--- StyleCop.Analyzers/StyleCop.Analyzers.CodeFixes/MaintainabilityRules/SA1402CodeFixProvider.cs
+++ StyleCop.Analyzers/StyleCop.Analyzers.CodeFixes/MaintainabilityRules/SA1402CodeFixProvider.cs
@@ -9,7 +9,6 @@ namespace StyleCop.Analyzers.MaintainabilityRules
     using System.Collections.Generic;
     using System.Collections.Immutable;
     using System.Composition;
-    using System.Diagnostics;
     using System.Linq;
     using System.Threading;
     using System.Threading.Tasks;
@@ -108,185 +107,38 @@ namespace StyleCop.Analyzers.MaintainabilityRules
             }
 
             // Add the new file
-            var semanticModel = await document.GetSemanticModelAsync(cancellationToken).ConfigureAwait(false);
-            var compilationUnit = (CompilationUnitSyntax)root;
+            SyntaxNode extractedDocumentNode = root.RemoveNodes(nodesToRemoveFromExtracted, SyntaxRemoveOptions.KeepUnbalancedDirectives);
+            Solution updatedSolution = document.Project.Solution.AddDocument(extractedDocumentId, extractedDocumentName, extractedDocumentNode, document.Folders);
 
-            // These are usings outside of the namespace
-            var allUsings = compilationUnit.Usings;
+            // Get unused usings and remove them from the extracted document
+            var newDoc = updatedSolution.GetDocument(extractedDocumentId);
+            var semanticModel = await newDoc.GetSemanticModelAsync(cancellationToken).ConfigureAwait(false);
+            var diagnostics = semanticModel.GetDiagnostics();
 
-            // Usings from within namespace
-            //foreach (var member in compilationUnit.Members.OfType<NamespaceDeclarationSyntax>())
-            //{
-            //    allUsings = allUsings.AddRange(member.Usings);
-            //}
+            var newRoot = await newDoc.GetSyntaxRootAsync(cancellationToken).ConfigureAwait(false);
+            var usingsToRemove = diagnostics
+                .Where(d => d.Id == "CS8019")
+                .Select(d => newRoot.FindNode(d.Location.SourceSpan))
+                .OfType<UsingDirectiveSyntax>()
+                .ToList();
 
-            // Gather all symbols referenced by the type being moved
-            TypeDeclarationSyntax typeDeclaration =
-                memberDeclarationSyntax as TypeDeclarationSyntax
-                ?? memberDeclarationSyntax.DescendantNodes().OfType<TypeDeclarationSyntax>().FirstOrDefault();
-
-            Debug.Assert(typeDeclaration.SyntaxTree == root.SyntaxTree, "The type declaration's syntax tree should match the root syntax tree.");
-
-            if (typeDeclaration == null)
+            if (usingsToRemove.Count > 0)
             {
-                return document.Project.Solution;
+                var cleanedRoot = newRoot.RemoveNodes(usingsToRemove, SyntaxRemoveOptions.KeepUnbalancedDirectives);
+                updatedSolution = updatedSolution.WithDocumentSyntaxRoot(extractedDocumentId, cleanedRoot);
             }
-
-            var walker = new ReferencedSymbolsWalker(semanticModel);
-            walker.Visit(typeDeclaration);
-            var referencedSymbols = walker.ReferencedSymbols;
-
-            var neededUsings = new List<UsingDirectiveSyntax>();
-            foreach (var usingDirective in allUsings)
-            {
-                if (IsUsingNeeded(usingDirective, referencedSymbols, semanticModel, typeDeclaration))
-                {
-                    neededUsings.Add(usingDirective);
-                }
-            }
-
-            // Build the new CompilationUnit for the extracted document
-            var originalNamespace = typeDeclaration.Parent as NamespaceDeclarationSyntax;
-
-            MemberDeclarationSyntax newTypeParented = typeDeclaration;
-            if (originalNamespace != null)
-            {
-                // If the type is inside a namespace, we need to create a new namespace declaration
-                newTypeParented = SyntaxFactory.NamespaceDeclaration(originalNamespace.Name)
-                    .WithMembers(SyntaxFactory.SingletonList<MemberDeclarationSyntax>(typeDeclaration))
-                    .WithLeadingTrivia(originalNamespace.GetLeadingTrivia())
-                    .WithTrailingTrivia(originalNamespace.GetTrailingTrivia());
-            }
-
-            var newCompilationUnit = SyntaxFactory.CompilationUnit()
-                    .WithUsings(SyntaxFactory.List(neededUsings))
-                    .WithMembers(SyntaxFactory.SingletonList(newTypeParented))
-                    .WithEndOfFileToken(compilationUnit.EndOfFileToken);
-
-            Solution updatedSolution = document.Project.Solution.AddDocument(extractedDocumentId, extractedDocumentName, newCompilationUnit, document.Folders);
 
             // Make sure to also add the file to linked projects
             foreach (var linkedDocumentId in document.GetLinkedDocumentIds())
             {
                 DocumentId linkedExtractedDocumentId = DocumentId.CreateNewId(linkedDocumentId.ProjectId);
-                updatedSolution = updatedSolution.AddDocument(linkedExtractedDocumentId, extractedDocumentName, newCompilationUnit, document.Folders);
+                updatedSolution = updatedSolution.AddDocument(linkedExtractedDocumentId, extractedDocumentName, extractedDocumentNode, document.Folders);
             }
 
             // Remove the type from its original location
             updatedSolution = updatedSolution.WithDocumentSyntaxRoot(document.Id, root.RemoveNode(node, SyntaxRemoveOptions.KeepUnbalancedDirectives));
 
             return updatedSolution;
-        }
-
-        private static bool IsUsingNeeded(UsingDirectiveSyntax usingDirective, IReadOnlyCollection<ISymbol> referencedSymbols, SemanticModel semanticModel, TypeDeclarationSyntax typeDeclaration)
-        {
-            // Normal using: check if any referenced symbol's namespace matches
-            var namespaceSymbol = semanticModel.GetSymbolInfo(usingDirective.Name).Symbol as INamespaceSymbol;
-            if (namespaceSymbol == null)
-            {
-                return false;
-            }
-
-            if (usingDirective.Alias != null)
-            {
-                // Check if the alias is used in the referenced symbols
-                var aliasName = usingDirective.Alias.Name.Identifier.ValueText;
-                return typeDeclaration.DescendantNodes()
-                    .OfType<IdentifierNameSyntax>()
-                    .Any(id => id.Identifier.ValueText == aliasName);
-            }
-
-            if (usingDirective.StaticKeyword.IsKind(SyntaxKind.StaticKeyword))
-            {
-                // Check if static members are used
-                var staticSymbol = semanticModel.GetSymbolInfo(usingDirective.Name).Symbol;
-                if (staticSymbol == null)
-                {
-                    return false;
-                }
-
-                // Check if any referenced symbol comes from this static type
-                return referencedSymbols.Any(rs => rs.ContainingType?.Equals(staticSymbol) == true);
-            }
-
-            return referencedSymbols.Any(rs =>
-            {
-                var containingNamespace = rs.ContainingNamespace;
-                while (containingNamespace != null && !containingNamespace.IsGlobalNamespace)
-                {
-                    if (containingNamespace.Equals(namespaceSymbol))
-                    {
-                        return true;
-                    }
-
-                    containingNamespace = containingNamespace.ContainingNamespace;
-                }
-
-                return false;
-            });
-        }
-
-        private class ReferencedSymbolsWalker : CSharpSyntaxWalker
-        {
-            private readonly SemanticModel semanticModel;
-            private readonly HashSet<ISymbol> referencedSymbols = new HashSet<ISymbol>();
-
-            public ReferencedSymbolsWalker(SemanticModel semanticModel)
-            {
-                this.semanticModel = semanticModel;
-            }
-
-            public IReadOnlyCollection<ISymbol> ReferencedSymbols => this.referencedSymbols.ToList();
-
-            public override void VisitIdentifierName(IdentifierNameSyntax node)
-            {
-                if (node.IsVar || node.IsMissing)
-                {
-                    return;
-                }
-
-                var symbol = this.semanticModel.GetSymbolInfo(node).Symbol;
-                Debug.WriteLine($"Visiting: {node.Identifier.Text}, Symbol: {symbol}");
-                Debug.Assert(symbol != null, $"Expected symbol for {node.Identifier.Text} to be found");
-                if (symbol != null && !symbol.IsImplicitlyDeclared)
-                {
-                    this.referencedSymbols.Add(symbol.OriginalDefinition);
-                }
-
-                base.VisitIdentifierName(node);
-            }
-
-            public override void VisitQualifiedName(QualifiedNameSyntax node)
-            {
-                if (node.IsMissing)
-                {
-                    return;
-                }
-
-                var symbol = this.semanticModel.GetSymbolInfo(node).Symbol;
-                if (symbol != null && !symbol.IsImplicitlyDeclared)
-                {
-                    this.referencedSymbols.Add(symbol.OriginalDefinition);
-                }
-
-                base.VisitQualifiedName(node);
-            }
-
-            public override void VisitMemberAccessExpression(MemberAccessExpressionSyntax node)
-            {
-                if (node.IsMissing)
-                {
-                    return;
-                }
-
-                var symbol = this.semanticModel.GetSymbolInfo(node).Symbol;
-                if (symbol != null && !symbol.IsImplicitlyDeclared)
-                {
-                    this.referencedSymbols.Add(symbol.OriginalDefinition);
-                }
-
-                base.VisitMemberAccessExpression(node);
-            }
         }
     }
 }
