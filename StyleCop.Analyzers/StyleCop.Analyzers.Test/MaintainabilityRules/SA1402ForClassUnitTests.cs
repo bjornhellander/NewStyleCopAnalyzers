0ea5da22fad1c0fccae493b8ae788d1e13ca514e
Matt Chaulklin <mchaulklin@gmail.com>
2025-08-13 13:50:09 -0400

    code cleanup

--- StyleCop.Analyzers/StyleCop.Analyzers.Test/MaintainabilityRules/SA1402ForClassUnitTests.cs
+++ StyleCop.Analyzers/StyleCop.Analyzers.Test/MaintainabilityRules/SA1402ForClassUnitTests.cs
@@ -5,16 +5,10 @@
 
 namespace StyleCop.Analyzers.Test.MaintainabilityRules
 {
-    using System.Threading;
-    using System.Threading.Tasks;
-    using Xunit;
-
     public class SA1402ForClassUnitTests : SA1402ForBlockDeclarationUnitTestsBase
     {
         public override string Keyword => "class";
 
         protected override bool IsConfiguredAsTopLevelTypeByDefault => true;
-
-        // Same tests but with usings outside of namespace
     }
 }
