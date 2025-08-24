f19e2cde6df8909becda654c0ffb04b6e7a61a40
Björn Hellander <bjorn.hellander@gmail.com>
2025-08-24 17:10:49 +0200

    Added a new test project for c# 14, temporarily using Roslyn 4.14.0 with language version 'preview'. Also updated some test cases where Roslyn's behavior had changed.

---
+++ StyleCop.Analyzers/StyleCop.Analyzers.Test.CSharp14/LayoutRules/SA1504CSharp14UnitTests.cs
@@ -0,0 +1,17 @@
+﻿// Copyright (c) Contributors to the New StyleCop Analyzers project.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+namespace StyleCop.Analyzers.Test.CSharp14.LayoutRules
+{
+    using System;
+    using Microsoft.CodeAnalysis.Testing;
+    using StyleCop.Analyzers.Test.CSharp13.LayoutRules;
+
+    public partial class SA1504CSharp14UnitTests : SA1504CSharp13UnitTests
+    {
+        protected override DiagnosticResult[] GetExpectedResultAccessorWithoutBody()
+        {
+            return Array.Empty<DiagnosticResult>();
+        }
+    }
+}
