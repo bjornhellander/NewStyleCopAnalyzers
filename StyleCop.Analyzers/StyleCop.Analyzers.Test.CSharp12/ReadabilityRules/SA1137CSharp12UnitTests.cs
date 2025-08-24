cfdf2d45e392ff1090454f18ca93a34086faacd1
Björn Hellander <bjorn.hellander@gmail.com>
2025-08-24 08:59:46 +0200

    Updated SA1137 to also check collection expressions #23

--- StyleCop.Analyzers/StyleCop.Analyzers.Test.CSharp12/ReadabilityRules/SA1137CSharp12UnitTests.cs
+++ StyleCop.Analyzers/StyleCop.Analyzers.Test.CSharp12/ReadabilityRules/SA1137CSharp12UnitTests.cs
@@ -3,9 +3,86 @@
 
 namespace StyleCop.Analyzers.Test.CSharp12.ReadabilityRules
 {
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Microsoft.CodeAnalysis.Testing;
     using StyleCop.Analyzers.Test.CSharp11.ReadabilityRules;
+    using Xunit;
+    using static StyleCop.Analyzers.Test.Verifiers.StyleCopCodeFixVerifier<
+        StyleCop.Analyzers.ReadabilityRules.SA1137ElementsShouldHaveTheSameIndentation,
+        StyleCop.Analyzers.ReadabilityRules.IndentationCodeFixProvider>;
 
     public partial class SA1137CSharp12UnitTests : SA1137CSharp11UnitTests
     {
+        [Fact]
+        public async Task TestSingleLineCollectionExpressionAsync()
+        {
+            string testCode = @"
+class TestClass
+{
+    private int[] testField = [1, 2, 3];
+}
+";
+
+            await VerifyCSharpDiagnosticAsync(testCode, DiagnosticResult.EmptyDiagnosticResults, CancellationToken.None).ConfigureAwait(false);
+        }
+
+        [Fact]
+        public async Task TestMultiLineCollectionExpressionStartingOnPreviousLineAsync()
+        {
+            string testCode = @"
+class TestClass
+{
+    private int[] testField =
+    [
+        1,
+[|         |]2,
+[|       |]3
+[|     |]];
+}
+";
+
+            string fixedCode = @"
+class TestClass
+{
+    private int[] testField =
+    [
+        1,
+        2,
+        3
+    ];
+}
+";
+
+            await VerifyCSharpFixAsync(testCode, DiagnosticResult.EmptyDiagnosticResults, fixedCode, CancellationToken.None).ConfigureAwait(false);
+        }
+
+        [Fact]
+        public async Task TestMultiLineCollectionExpressionStartingOnNextLineAsync()
+        {
+            string testCode = @"
+class TestClass
+{
+    private int[] testField = [
+        1,
+[|         |]2,
+[|       |]3
+    ];
+}
+";
+
+            string fixedCode = @"
+class TestClass
+{
+    private int[] testField = [
+        1,
+        2,
+        3
+    ];
+}
+";
+
+            await VerifyCSharpFixAsync(testCode, DiagnosticResult.EmptyDiagnosticResults, fixedCode, CancellationToken.None).ConfigureAwait(false);
+        }
     }
 }
