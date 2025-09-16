e7b7eaba48334a234cb4e88dcfbe26b35f5a860d
Björn Hellander <bjorn.hellander@gmail.com>
2025-09-16 08:19:28 +0200

    Update SA1101PrefixLocalCallsWithThis to not trigger in extension members #30

--- StyleCop.Analyzers/StyleCop.Analyzers.Test.CSharp14/ReadabilityRules/SA1101CSharp14UnitTests.cs
+++ StyleCop.Analyzers/StyleCop.Analyzers.Test.CSharp14/ReadabilityRules/SA1101CSharp14UnitTests.cs
@@ -3,9 +3,63 @@
 
 namespace StyleCop.Analyzers.Test.CSharp14.ReadabilityRules
 {
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Microsoft.CodeAnalysis.Testing;
     using StyleCop.Analyzers.Test.CSharp13.ReadabilityRules;
+    using Xunit;
+    using static StyleCop.Analyzers.Test.Verifiers.StyleCopCodeFixVerifier<
+        StyleCop.Analyzers.ReadabilityRules.SA1101PrefixLocalCallsWithThis,
+        StyleCop.Analyzers.ReadabilityRules.SA1101CodeFixProvider>;
 
     public partial class SA1101CSharp14UnitTests : SA1101CSharp13UnitTests
     {
+        [Fact]
+        public async Task TestExtensionMemberPropertyAsync()
+        {
+            var testCode = @"
+public static class TestClass
+{
+    extension(string source)
+    {
+        public int MyLength => source.Length;
+    }
+}
+";
+
+            await VerifyCSharpDiagnosticAsync(testCode, DiagnosticResult.EmptyDiagnosticResults, CancellationToken.None).ConfigureAwait(false);
+        }
+
+        [Fact]
+        public async Task TestExtensionMemberIndexerAsync()
+        {
+            var testCode = @"
+public static class TestClass
+{
+    extension(object source)
+    {
+        public object? this[int index] => source;
+    }
+}
+";
+
+            await VerifyCSharpDiagnosticAsync(testCode, DiagnosticResult.EmptyDiagnosticResults, CancellationToken.None).ConfigureAwait(false);
+        }
+
+        [Fact]
+        public async Task TestExtensionMemberMethodAsync()
+        {
+            var testCode = @"
+public static class TestClass
+{
+    extension(string source)
+    {
+        public int GetMyLength() => source.Length;
+    }
+}
+";
+
+            await VerifyCSharpDiagnosticAsync(testCode, DiagnosticResult.EmptyDiagnosticResults, CancellationToken.None).ConfigureAwait(false);
+        }
     }
 }
