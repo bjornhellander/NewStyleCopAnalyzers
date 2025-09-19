a96df50729b2bd2fca960075b66b559af8447bbd
Björn Hellander <bjorn.hellander@gmail.com>
2025-09-19 08:08:22 +0200

    Update SA1500BracesForMultiLineStatementsMustNotShareLine to handle extension declarations #30

--- StyleCop.Analyzers/StyleCop.Analyzers.Test.CSharp14/LayoutRules/SA1500CSharp14UnitTests.cs
+++ StyleCop.Analyzers/StyleCop.Analyzers.Test.CSharp14/LayoutRules/SA1500CSharp14UnitTests.cs
@@ -3,9 +3,45 @@
 
 namespace StyleCop.Analyzers.Test.CSharp14.LayoutRules
 {
+    using System.Threading;
+    using System.Threading.Tasks;
     using StyleCop.Analyzers.Test.CSharp13.LayoutRules;
+    using Xunit;
+    using static StyleCop.Analyzers.Test.Verifiers.StyleCopCodeFixVerifier<
+        StyleCop.Analyzers.LayoutRules.SA1500BracesForMultiLineStatementsMustNotShareLine,
+        StyleCop.Analyzers.LayoutRules.SA1500CodeFixProvider>;
 
     public partial class SA1500CSharp14UnitTests : SA1500CSharp13UnitTests
     {
+        [Fact]
+        public async Task TestExtensionDeclarationAsync()
+        {
+            var testCode = @"
+public static class TestClass
+{
+    extension(string source) {
+    }
+}
+";
+
+            var fixedCode = @"
+public static class TestClass
+{
+    extension(string source)
+    {
+    }
+}
+";
+
+            // TODO: Syntax node actions seems to be triggered twice
+            // Reported in https://github.com/dotnet/roslyn/issues/80319
+            var expected = new[]
+            {
+                Diagnostic().WithSpan(4, 30, 4, 31),
+                Diagnostic().WithSpan(4, 30, 4, 31),
+            };
+
+            await VerifyCSharpFixAsync(testCode, expected, fixedCode, CancellationToken.None).ConfigureAwait(false);
+        }
     }
 }
