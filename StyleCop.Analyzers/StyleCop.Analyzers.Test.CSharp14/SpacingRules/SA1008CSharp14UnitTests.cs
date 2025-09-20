79a9599c8fb091dcc7278d11ca3cbad53258750c
Björn Hellander <bjorn.hellander@gmail.com>
2025-09-20 18:08:31 +0200

    Add SA1008OpeningParenthesisMustBeSpacedCorrectly tests for extension declarations #30

--- StyleCop.Analyzers/StyleCop.Analyzers.Test.CSharp14/SpacingRules/SA1008CSharp14UnitTests.cs
+++ StyleCop.Analyzers/StyleCop.Analyzers.Test.CSharp14/SpacingRules/SA1008CSharp14UnitTests.cs
@@ -3,9 +3,67 @@
 
 namespace StyleCop.Analyzers.Test.CSharp14.SpacingRules
 {
+    using System.Threading;
+    using System.Threading.Tasks;
     using StyleCop.Analyzers.Test.CSharp13.SpacingRules;
+    using Xunit;
+    using static StyleCop.Analyzers.SpacingRules.SA1008OpeningParenthesisMustBeSpacedCorrectly;
+    using static StyleCop.Analyzers.Test.Verifiers.StyleCopCodeFixVerifier<
+        StyleCop.Analyzers.SpacingRules.SA1008OpeningParenthesisMustBeSpacedCorrectly,
+        StyleCop.Analyzers.SpacingRules.TokenSpacingCodeFixProvider>;
 
     public partial class SA1008CSharp14UnitTests : SA1008CSharp13UnitTests
     {
+        [Fact]
+        public async Task TestExtensionDeclarationWithSpaceBeforeParenthesisAsync()
+        {
+            var testCode = @"
+public static class TestClass
+{
+    extension {|#0:(|}string source)
+    {
+    }
+}
+";
+
+            var fixedCode = @"
+public static class TestClass
+{
+    extension(string source)
+    {
+    }
+}
+";
+
+            var expected = Diagnostic(DescriptorNotPreceded).WithLocation(0);
+
+            await VerifyCSharpFixAsync(testCode, expected, fixedCode, CancellationToken.None).ConfigureAwait(false);
+        }
+
+        [Fact]
+        public async Task TestExtensionDeclarationWithSpaceAfterParenthesisAsync()
+        {
+            var testCode = @"
+public static class TestClass
+{
+    extension{|#0:(|} string source)
+    {
+    }
+}
+";
+
+            var fixedCode = @"
+public static class TestClass
+{
+    extension(string source)
+    {
+    }
+}
+";
+
+            var expected = Diagnostic(DescriptorNotFollowed).WithLocation(0);
+
+            await VerifyCSharpFixAsync(testCode, expected, fixedCode, CancellationToken.None).ConfigureAwait(false);
+        }
     }
 }
