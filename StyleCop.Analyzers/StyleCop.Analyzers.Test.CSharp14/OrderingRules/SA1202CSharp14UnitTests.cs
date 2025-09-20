9a60dc82100f1e601d68fb3bc5cc26faec03d0e2
Björn Hellander <bjorn.hellander@gmail.com>
2025-09-20 08:47:26 +0200

    Add SA1202ElementsMustBeOrderedByAccess tests for extension declarations #30

--- StyleCop.Analyzers/StyleCop.Analyzers.Test.CSharp14/OrderingRules/SA1202CSharp14UnitTests.cs
+++ StyleCop.Analyzers/StyleCop.Analyzers.Test.CSharp14/OrderingRules/SA1202CSharp14UnitTests.cs
@@ -3,9 +3,102 @@
 
 namespace StyleCop.Analyzers.Test.CSharp14.OrderingRules
 {
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Microsoft.CodeAnalysis.Testing;
     using StyleCop.Analyzers.Test.CSharp13.OrderingRules;
+    using Xunit;
+    using static StyleCop.Analyzers.Test.Verifiers.StyleCopCodeFixVerifier<
+        StyleCop.Analyzers.OrderingRules.SA1202ElementsMustBeOrderedByAccess,
+        StyleCop.Analyzers.OrderingRules.ElementOrderCodeFixProvider>;
 
     public partial class SA1202CSharp14UnitTests : SA1202CSharp13UnitTests
     {
+        [Fact]
+        public async Task TestPropertyAfterExtensionDeclarationAsync()
+        {
+            string testCode = @"
+public static class TestClass
+{
+    extension(string source)
+    {
+    }
+
+    public static int TestProperty => 0;
+}
+";
+
+            await VerifyCSharpDiagnosticAsync(testCode, DiagnosticResult.EmptyDiagnosticResults, CancellationToken.None).ConfigureAwait(false);
+        }
+
+        [Fact]
+        public async Task TestPropertyBeforeExtensionDeclarationAsync()
+        {
+            string testCode = @"
+public static class TestClass
+{
+    public static int TestProperty => 0;
+
+    extension(string source)
+    {
+    }
+}
+";
+
+            await VerifyCSharpDiagnosticAsync(testCode, DiagnosticResult.EmptyDiagnosticResults, CancellationToken.None).ConfigureAwait(false);
+        }
+
+        [Fact]
+        public async Task TestFieldBeforeExtensionDeclarationAsync()
+        {
+            string testCode = @"
+public static class TestClass
+{
+    private static int TestField;
+
+    extension(string source)
+    {
+    }
+}
+";
+
+            await VerifyCSharpDiagnosticAsync(testCode, DiagnosticResult.EmptyDiagnosticResults, CancellationToken.None).ConfigureAwait(false);
+        }
+
+        [Fact]
+        public async Task TestFieldAfterExtensionDeclarationAsync()
+        {
+            string testCode = @"
+public static class TestClass
+{
+    extension(string source)
+    {
+    }
+
+    private static int TestField;
+}
+";
+
+            await VerifyCSharpDiagnosticAsync(testCode, DiagnosticResult.EmptyDiagnosticResults, CancellationToken.None).ConfigureAwait(false);
+        }
+
+        [Fact]
+        public async Task TestTwoExtensionDeclarationsAsync()
+        {
+            string testCode = @"
+public static class TestClass
+{
+    extension(string source)
+    {
+    }
+
+    extension(string source)
+    {
+    }
+}
+";
+
+            await VerifyCSharpDiagnosticAsync(testCode, DiagnosticResult.EmptyDiagnosticResults, CancellationToken.None).ConfigureAwait(false);
+        }
     }
 }
