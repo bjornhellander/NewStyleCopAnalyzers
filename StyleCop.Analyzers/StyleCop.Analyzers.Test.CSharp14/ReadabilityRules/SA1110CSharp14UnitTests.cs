5c76d77f51028c07aed9ae860c60b11a87ea4379
Björn Hellander <bjorn.hellander@gmail.com>
2025-09-20 09:10:18 +0200

    Update SA1110OpeningParenthesisMustBeOnDeclarationLine to handle extension declarations #30

--- StyleCop.Analyzers/StyleCop.Analyzers.Test.CSharp14/ReadabilityRules/SA1110CSharp14UnitTests.cs
+++ StyleCop.Analyzers/StyleCop.Analyzers.Test.CSharp14/ReadabilityRules/SA1110CSharp14UnitTests.cs
@@ -3,9 +3,46 @@
 
 namespace StyleCop.Analyzers.Test.CSharp14.ReadabilityRules
 {
+    using System.Threading;
+    using System.Threading.Tasks;
     using StyleCop.Analyzers.Test.CSharp13.ReadabilityRules;
+    using Xunit;
+    using static StyleCop.Analyzers.Test.Verifiers.StyleCopCodeFixVerifier<
+        StyleCop.Analyzers.ReadabilityRules.SA1110OpeningParenthesisMustBeOnDeclarationLine,
+        StyleCop.Analyzers.SpacingRules.TokenSpacingCodeFixProvider>;
 
     public partial class SA1110CSharp14UnitTests : SA1110CSharp13UnitTests
     {
+        [Fact]
+        public async Task TestExtensionDeclarationAsync()
+        {
+            string testCode = @"
+public static class TestClass
+{
+    extension
+        {|#0:(|}string source)
+    {
+    }
+}
+";
+
+            string fixedCode = @"
+public static class TestClass
+{
+    extension(
+        string source)
+    {
+    }
+}
+";
+
+            var expected = new[]
+            {
+                Diagnostic().WithLocation(0),
+                Diagnostic().WithLocation(0),
+            };
+
+            await VerifyCSharpFixAsync(testCode, expected, fixedCode, CancellationToken.None).ConfigureAwait(false);
+        }
     }
 }
