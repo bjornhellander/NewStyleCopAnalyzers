e61416ebfcad76efcd38589c3ed7bd7477323153
Björn Hellander <bjorn.hellander@gmail.com>
2025-09-20 19:04:16 +0200

    Add SA1314TypeParameterNamesMustBeginWithT tests for extension declarations #30

--- StyleCop.Analyzers/StyleCop.Analyzers.Test.CSharp14/NamingRules/SA1314CSharp14UnitTests.cs
+++ StyleCop.Analyzers/StyleCop.Analyzers.Test.CSharp14/NamingRules/SA1314CSharp14UnitTests.cs
@@ -3,9 +3,46 @@
 
 namespace StyleCop.Analyzers.Test.CSharp14.NamingRules
 {
+    using System.Threading;
+    using System.Threading.Tasks;
     using StyleCop.Analyzers.Test.CSharp13.NamingRules;
+    using Xunit;
+    using static StyleCop.Analyzers.Test.Verifiers.StyleCopCodeFixVerifier<
+        StyleCop.Analyzers.NamingRules.SA1314TypeParameterNamesMustBeginWithT,
+        StyleCop.Analyzers.NamingRules.SA1314CodeFixProvider>;
 
     public partial class SA1314CSharp14UnitTests : SA1314CSharp13UnitTests
     {
+        [Fact]
+        public async Task TestExtensionDeclarationeAsync()
+        {
+            var testCode = @"
+public static class TestClass
+{
+    extension<{|#0:Obj|}>(Obj param)
+    {
+    }
+}
+";
+
+            var fixedCode = @"
+public static class TestClass
+{
+    extension<TObj>(TObj param)
+    {
+    }
+}
+";
+
+            // TODO: Syntax node actions seems to be triggered twice
+            // Reported in https://github.com/dotnet/roslyn/issues/80319
+            var expected = new[]
+            {
+                Diagnostic().WithLocation(0),
+                Diagnostic().WithLocation(0),
+            };
+
+            await VerifyCSharpFixAsync(testCode, expected, fixedCode, CancellationToken.None).ConfigureAwait(false);
+        }
     }
 }
