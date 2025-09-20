f6db474077bc185f63acccc1350b8bc0e68808c9
Björn Hellander <bjorn.hellander@gmail.com>
2025-09-20 19:43:44 +0200

    Add SA1513ClosingBraceMustBeFollowedByBlankLine tests for extension declarations #30

--- StyleCop.Analyzers/StyleCop.Analyzers.Test.CSharp14/LayoutRules/SA1513CSharp14UnitTests.cs
+++ StyleCop.Analyzers/StyleCop.Analyzers.Test.CSharp14/LayoutRules/SA1513CSharp14UnitTests.cs
@@ -3,9 +3,47 @@
 
 namespace StyleCop.Analyzers.Test.CSharp14.LayoutRules
 {
+    using System.Threading;
+    using System.Threading.Tasks;
     using StyleCop.Analyzers.Test.CSharp13.LayoutRules;
+    using Xunit;
+    using static StyleCop.Analyzers.Test.Verifiers.StyleCopCodeFixVerifier<
+        StyleCop.Analyzers.LayoutRules.SA1513ClosingBraceMustBeFollowedByBlankLine,
+        StyleCop.Analyzers.LayoutRules.SA1513CodeFixProvider>;
 
     public partial class SA1513CSharp14UnitTests : SA1513CSharp13UnitTests
     {
+        [Fact]
+        public async Task TestExtensionDeclarationAsync()
+        {
+            var testCode = @"
+public static class TestClass
+{
+    extension(string source)
+    {
+    }
+    extension(string source)
+    {
+    }
+}
+";
+
+            var fixedCode = @"
+public static class TestClass
+{
+    extension(string source)
+    {
+    }
+
+    extension(string source)
+    {
+    }
+}
+";
+
+            var expected = Diagnostic().WithSpan(6, 6, 7, 1);
+
+            await VerifyCSharpFixAsync(testCode, expected, fixedCode, CancellationToken.None).ConfigureAwait(false);
+        }
     }
 }
