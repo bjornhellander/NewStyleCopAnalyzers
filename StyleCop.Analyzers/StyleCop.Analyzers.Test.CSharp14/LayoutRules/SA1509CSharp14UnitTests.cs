baeceadeb73bc344aa9253f26b6b939e388f3115
Björn Hellander <bjorn.hellander@gmail.com>
2025-09-20 19:33:35 +0200

    Add SA1509OpeningBracesMustNotBePrecededByBlankLine tests for extension declarations #30

--- StyleCop.Analyzers/StyleCop.Analyzers.Test.CSharp14/LayoutRules/SA1509CSharp14UnitTests.cs
+++ StyleCop.Analyzers/StyleCop.Analyzers.Test.CSharp14/LayoutRules/SA1509CSharp14UnitTests.cs
@@ -3,9 +3,41 @@
 
 namespace StyleCop.Analyzers.Test.CSharp14.LayoutRules
 {
+    using System.Threading;
+    using System.Threading.Tasks;
     using StyleCop.Analyzers.Test.CSharp13.LayoutRules;
+    using Xunit;
+    using static StyleCop.Analyzers.Test.Verifiers.StyleCopCodeFixVerifier<
+        StyleCop.Analyzers.LayoutRules.SA1509OpeningBracesMustNotBePrecededByBlankLine,
+        StyleCop.Analyzers.LayoutRules.SA1509CodeFixProvider>;
 
     public partial class SA1509CSharp14UnitTests : SA1509CSharp13UnitTests
     {
+        [Fact]
+        public async Task TestExtensionDeclarationAsync()
+        {
+            var testCode = @"
+public static class TestClass
+{
+    extension(string source)
+
+    {|#0:{|}
+    }
+}
+";
+
+            var fixedCode = @"
+public static class TestClass
+{
+    extension(string source)
+    {
+    }
+}
+";
+
+            var expected = Diagnostic().WithLocation(0);
+
+            await VerifyCSharpFixAsync(testCode, expected, fixedCode, CancellationToken.None).ConfigureAwait(false);
+        }
     }
 }
