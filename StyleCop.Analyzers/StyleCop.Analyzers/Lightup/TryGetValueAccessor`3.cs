db41328bceb702481b3cfb70ace01eba32d41422
Björn Hellander <bjorn.hellander@gmail.com>
2026-07-11 06:34:42 +0200

    Remove existing lightup

--- StyleCop.Analyzers/StyleCop.Analyzers/Lightup/TryGetValueAccessor`3.cs
+++
@@ -1,9 +0,0 @@
-﻿// Copyright (c) Contributors to the New StyleCop Analyzers project.
-// Licensed under the MIT License. See LICENSE in the project root for license information.
-
-#nullable disable
-
-namespace StyleCop.Analyzers.Lightup
-{
-    internal delegate bool TryGetValueAccessor<T, TKey, TValue>(T instance, TKey key, out TValue value);
-}
